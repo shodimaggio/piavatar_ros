297018a249324138c4a724d27cde7a3e7bf2aabb
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2017-07-30 06:24:02 +0900

    Add a test script

---
+++ test/travis_test_buzzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy, unittest, rostest
+import rosnode
+import time
+
+class BuzzerTest(unittest.TestCase):
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('/buzzer',nodes,"node does not exist")
+
+if __name__ == '__main__':
+    time.sleep(3)
+    rospy.init_node('travis_test_buzzer')
+    rostest.rosrun('piavatar_ros','travis_test_buzzer',BuzzerTest)
+
