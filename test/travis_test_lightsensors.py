1919db8607d35371f96338195824de2cdddd740e
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2017-08-04 08:12:27 +0900

    Reivse a file

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -15,7 +15,7 @@ class LightSensorTest(unittest.TestCase):
   	self.count += 1
 	self.values = data
 
-    def check_values(self,lf,ls,rs,rf)
+    def check_values(self,lf,ls,rs,rf):
 	vs = self.values
 	self.assertEqual(vs.left_forward, lf, "different value: left_forward")
 	self.assertEqual(vs.left_side, ls, "different value: left_side")
